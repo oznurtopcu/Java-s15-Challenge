d23f8455069b910882f3d51508a88f9f30ade87d
oznurtopcu <topcuoznur121@gmail.com>
2025-04-05 00:22:19 +0300

    create author class

---
+++ src/Author.java
@@ -0,0 +1,11 @@
+public class Author extends Person{
+
+    public Author(String name){
+        super(name);
+    }
+
+    @Override
+    public void whoyouare() {
+        System.out.println("I am an author.");
+    }
+}
