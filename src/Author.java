ff8a16d9e7befeeb19b4412daf49f873683771ad
oznurtopcu <topcuoznur121@gmail.com>
2025-04-05 16:00:07 +0300

    author methods completed

--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,9 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Author extends Person{
+
+    private Set<Book> booksWritten = new HashSet<>();
 
     public Author(String name){
         super(name);
@@ -7,5 +12,20 @@ public class Author extends Person{
     @Override
     public void whoyouare() {
         System.out.println("I am an author. My name is" + getName());
+    }
+
+    public void newBook(Book book) {
+
+        if(book.getAuthor().getName().equals(getName())) {
+            booksWritten.add(book);
+        }
+
+    }
+
+    public void showBook() {
+        System.out.println(getName() + "'s Book List: ");
+        for(Book book: booksWritten) {
+            System.out.println(" - " + book.getName());
+        }
     }
 }
