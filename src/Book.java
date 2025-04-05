b97d2fd9e499acd0417ec2a35229e3f40e452b99
oznurtopcu <topcuoznur121@gmail.com>
2025-04-05 15:46:12 +0300

    book methods completed

--- src/Book.java
+++ src/Book.java
@@ -1,38 +1,72 @@
+import java.time.LocalDate;
+
 public abstract class Book {
     private long id;
-    private String author;
+    private Author author;
     private String name;
-    private boolean isBorrowed;
-    private String date;
-    private String owner;
+    private BookStatus status;
+    private LocalDate date;
+    private Reader owner;
 
-    public Book(long id, String author, String name) {
+    public Book(long id, Author author, String name) {
         this.id = id;
         this.author = author;
         this.name = name;
-        this.isBorrowed = false;
-
+        this.status = BookStatus.AVAILABLE;
+        this.date = LocalDate.now();
     }
 
-    public String getAuthor() {
+    public long getId() {
+        return id;
+    }
+    public Author getAuthor() {
         return author;
     }
     public String getName() {
         return name;
     }
-    public String getOwner() {
+    public BookStatus getStatus() {
+        return status;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public Reader getOwner() {
         return owner;
     }
-    public void changeOwner(String owner) {
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public void changeOwner(Reader owner) {
         this.owner = owner;
     }
-    public void updateStatus() {
-        this.isBorrowed = !isBorrowed;
+    public void updateStatus(BookStatus status) {
+        this.status = status;
     }
-    public void displayInfo() {
-        System.out.println( "Kitap ID: " + id + " " +
-                "Kitap adı: " + name + " " +
-                "Kitap Yazarı: " + author
-                );
+    public abstract void displayInfo();
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", date=" + date +
+                ", owner='" + owner + '\'' +
+                '}';
     }
 }
