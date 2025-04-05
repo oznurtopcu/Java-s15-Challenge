57b7b5f1413c74702d1766eb4f69fffa5f20b241
oznurtopcu <topcuoznur121@gmail.com>
2025-04-05 14:38:18 +0300

    add book methods

--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Library {
 
     public static final String NAME = "Workintech Library"; //const
 
     private static Library instance;
 
-    private Library() {
+    private List<Book> books;
+    private List<Author> authors;
 
+    private Library() {
+        this.books = new ArrayList<>();
     }
 
     public static Library getInstance() {
@@ -13,5 +20,24 @@ public class Library {
             instance = new Library();
         }
         return instance;
+    }
+
+    public void addBook(Book book) {
+        if(books == null) {
+            books = new ArrayList<>();
+        }
+        if(!books.contains(book))
+            books.add(book);
+
+        for(Book showbook: books) {
+            showbook.displayInfo();
+        }
+        System.out.println("Kitap ekleme işlemi başarılı!");
+    }
+
+    public List<Book> getBooks() {
+        return books.stream().collect(Collectors.toUnmodifiableList());
+        //stream'e dönüştürerek listenin bir kopyasını gönderdik
+        //salt okunabilir, değiştirilemez, set edilemez
     }
 }
