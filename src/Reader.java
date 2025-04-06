5f7798568b19e3f3ce44cb6c313c230cfe6a7d41
oznurtopcu <topcuoznur121@gmail.com>
2025-04-06 13:57:23 +0300

    create methods for reader class

--- src/Reader.java
+++ src/Reader.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reader extends Person{
+
+    private List<Book> borrowedBooks = new ArrayList<>();
 
     public Reader(String name) {
         super(name);
@@ -8,4 +13,42 @@ public class Reader extends Person{
     public void whoyouare() {
         System.out.println("I am a reader. My name is" + getName());
     }
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public boolean purchaseBook(Book book) {
+        if(book.getStatus().equals(BookStatus.AVAILABLE)) {
+            book.updateStatus(BookStatus.SOLD);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean borrowBook(Book book) {
+        if(borrowedBooks.size()<5 && book.getStatus().equals(BookStatus.AVAILABLE)) {
+            borrowedBooks.add(book);
+            book.updateStatus(BookStatus.BORROWED);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean returnBook(Book book) {
+        if(book.getStatus().equals(BookStatus.BORROWED)) {
+            book.updateStatus(BookStatus.AVAILABLE);
+            borrowedBooks.remove(book);
+            return true;
+        }
+        return false;
+
+    }
+
+    public void showBook() {
+        System.out.println("Borrowed book list: ");
+        for(Book book: borrowedBooks) {
+            System.out.println(book.toString());
+        }
+    }
+
 }
