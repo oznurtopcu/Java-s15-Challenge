720ff5d8a4c02c15838a1b90992c07c9ababf619
oznurtopcu <topcuoznur121@gmail.com>
2025-04-06 14:18:36 +0300

    add methods for library class

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Library {
@@ -9,11 +7,14 @@ public class Library {
 
     private static Library instance;
 
-    private List<Book> books;
-    private Set<Author> authors;
+    private Map<Long,Book> books;
+    private List<Reader> readers;
+//    private Map<Long, String> librarians;
 
     private Library() {
-        this.books = new ArrayList<>();
+        this.books = new HashMap<>();
+        this.readers = new ArrayList<>();
+//        this.librarians = new LinkedHashMap<>();
     }
 
     public static Library getInstance() {
@@ -22,23 +23,90 @@ public class Library {
         }
         return instance;
     }
+//    BIR ONCEKI COMMITTEN KALAN KODLAR
+//
+//    public void addBook(Book book) {
+//        if(books == null) {
+//            books = new ArrayList<>();
+//        }
+//        if(!books.contains(book))
+//            books.add(book);
+//
+//        for(Book showbook: books) {
+//            showbook.displayInfo();
+//        }
+//        System.out.println("Kitap ekleme işlemi başarılı!");
+//    }
+//
+//    public List<Book> getBooks() {
+//        return books.stream().collect(Collectors.toUnmodifiableList());
+//        //stream'e dönüştürerek listenin bir kopyasını gönderdik
+//        //salt okunabilir, değiştirilemez, set edilemez
+//    }
+//
+//
 
     public void addBook(Book book) {
-        if(books == null) {
-            books = new ArrayList<>();
-        }
-        if(!books.contains(book))
-            books.add(book);
-
-        for(Book showbook: books) {
-            showbook.displayInfo();
-        }
+        if(!books.containsKey(book.getId()))
+            books.put(book.getId(), book);
         System.out.println("Kitap ekleme işlemi başarılı!");
     }
 
-    public List<Book> getBooks() {
-        return books.stream().collect(Collectors.toUnmodifiableList());
-        //stream'e dönüştürerek listenin bir kopyasını gönderdik
-        //salt okunabilir, değiştirilemez, set edilemez
+    public void lendBook(Book book, Reader reader) {
+        reader.borrowBook(book);
     }
+
+    public boolean takeBackBook(Book book, Reader reader) {
+       return reader.returnBook(book);
+    }
+
+    public Map<Long, Book> getBooks(){
+        return Collections.unmodifiableMap(books);
+    }
+
+    public List<Reader> getReaders() {
+        return Collections.unmodifiableList(readers);
+    }
+
+
+//    public List<Book> getBook(String bookName) {
+//        List<Book> matching = new ArrayList<>();
+//
+//        for (Book book: books.values()) {
+//            if(book.getName().equalsIgnoreCase(bookName)) {
+//                matching.add(book);
+//            }
+//        }
+//        return  matching;
+//    }
+
+//    public Book getBookById(long id) {
+//        return books.get(id);
+//    }
+//
+//    public List<Book> getBookByName(String name) {
+//        List<Book> matching = new ArrayList<>();
+//
+//        for (Book book: books.values()) {
+//            if(book.getName().equalsIgnoreCase(name)) {
+//                matching.add(book);
+//            }
+//        }
+//        return matching;
+//    }
+//
+//    public List<Book> getBookByAuthor(String author) {
+//        List<Book> matching = new ArrayList<>();
+//
+//        for (Book book: books.values()) {
+//            if(book.getAuthor().getName().equalsIgnoreCase(author)) {
+//                matching.add(book);
+//            }
+//        }
+//        return matching;
+//    }
+
+
+
+
 }
